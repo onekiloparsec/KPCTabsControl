dc10dafeaad50be06944b29f745205cb533ee4b5
Cédric Foellmi <cedric@onekilopars.ec>
2015-04-09 10:02:36 +0200

    Making the framework target

---
+++ KPCTabsControlTests/KPCTabsControlTests.m
@@ -0,0 +1,40 @@
+//
+//  KPCTabsControlTests.m
+//  KPCTabsControlTests
+//
+//  Created by Cédric Foellmi on 09/04/15.
+//  Copyright (c) 2015 Cédric Foellmi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface KPCTabsControlTests : XCTestCase
+
+@end
+
+@implementation KPCTabsControlTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
