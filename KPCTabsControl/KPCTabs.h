7dbda4ae465ef31982af26e2e289e1cdfd82ffd9
Cédric Foellmi <cedric@onekilopars.ec>
2015-12-21 17:01:11 +0100

    Adding umbrella header

---
+++ KPCTabsControl/KPCTabs.h
@@ -0,0 +1,23 @@
+//
+//  KPCTabs.h
+//  KPCTabsControl
+//
+//  Created by Cédric Foellmi on 21/12/15.
+//  Copyright © 2015 Cédric Foellmi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KPCTabs.
+FOUNDATION_EXPORT double KPCTabsVersionNumber;
+
+//! Project version string for KPCTabs.
+FOUNDATION_EXPORT const unsigned char KPCTabsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KPCTabs/PublicHeader.h>
+
+#import <KPCTabsControl/KPCTabsControlConstants.h>
+#import <KPCTabsControl/KPCTabsControlProtocols.h>
+#import <KPCTabsControl/KPCTabsControl.h>
+#import <KPCTabsControl/KPCTabButton.h>
+#import <KPCTabsControl/KPCTabButtonCell.h>
