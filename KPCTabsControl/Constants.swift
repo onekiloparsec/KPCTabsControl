84ba08fb0ea4400c89ee2fa0765dc743c1100a27
Cédric <cedric@onekilopars.ec>
2016-08-17 22:29:14 +0200

    Some documentation

--- KPCTabsControl/Constants.swift
+++ KPCTabsControl/Constants.swift
@@ -8,8 +8,16 @@
 
 import Foundation
 
+/// The name of the notification upon the selection of a new tab.
 public let TabsControlSelectionDidChangeNotification = "TabsControlSelectionDidChangeNotification"
 
+/**
+ The position of a tab button inside the control. Used in the Style.
+ 
+ - first:  The most left-hand tab button.
+ - middle: Any middle tab button between first and last.
+ - last:   The most right-hand tab button
+ */
 public enum TabButtonPosition {
     case first
     case middle
@@ -18,21 +26,27 @@ public enum TabButtonPosition {
 
 public typealias Offset = NSPoint
 
-extension Offset {
+public extension Offset {
     
     public init(x: CGFloat) {
-        
         self.x = x
         self.y = 0
     }
     
     public init(y: CGFloat) {
-        
         self.x = 0
         self.y = y
     }
 }
 
+/**
+ Addition operator to NSPoints and Offsets.
+ 
+ - parameter lhs: lef-hand side point
+ - parameter rhs: right-hand side offset to be added to the point.
+ 
+ - returns: A new and offset NSPoint. 
+ */
 public func +(lhs: NSPoint, rhs: Offset) -> NSPoint {
     return NSPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
 }
