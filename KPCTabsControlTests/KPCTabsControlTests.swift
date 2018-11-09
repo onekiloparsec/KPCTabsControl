1c3f2bcffc559c1cfb3d6660ae5bba93bc9922c0
Cédric Foellmi <cedric@onekilopars.ec>
2018-11-09 12:55:20 +0100

    Create test target for letting travis succeed

---
+++ KPCTabsControlTests/KPCTabsControlTests.swift
@@ -0,0 +1,33 @@
+//
+//  KPCTabsControlTests.swift
+//  KPCTabsControlTests
+//
+//  Created by onekiloparsec on 09/11/2018.
+//  Copyright © 2018 Cédric Foellmi. All rights reserved.
+//
+
+import XCTest
+
+class KPCTabsControlTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
