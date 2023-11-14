6fb119cd7a5be3c7c2226bbfce86c516fed803a4
Marin Todorov <marin@underplot.com>
2023-11-14 08:11:57 +0100

    fixes resources under SPM

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,8 +21,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "KPCTabsControl",
-            dependencies: [],
-            path: "KPCTabsControl"
+            path: "KPCTabsControl",
+            resources: [
+                .copy("Resources")
+            ],
+            swiftSettings: [.define("SwiftPackage")]
         )
     ]
 )
